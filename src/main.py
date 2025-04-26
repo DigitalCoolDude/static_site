3fce759f31667b482bcb82cb22d8f4363c5c8e08
Red <DigitalCoolDude@hotmail.com>
2025-04-26 02:52:11 -0600

    CH5 L1: Copy Static Complete

--- src/main.py
+++ src/main.py
@@ -1,7 +1,23 @@
+import os, shutil
 from textnode import TextNode, TextType
 
 def main():
-   ex = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-   print(ex)
+   # ex = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
+   # print(ex)
+   copy()
+
+def copy():
+   if os.path.exists("public"):
+      shutil.rmtree("public")
+
+   dir_list = [("static", "public")]
+   
+   for s_item, p_item in dir_list:
+      if os.path.isfile(s_item):
+         shutil.copy(s_item, p_item)
+      else:
+         os.mkdir(p_item)
+         for sub in os.listdir(s_item):
+            dir_list.append((os.path.join(s_item, sub), os.path.join(p_item, sub)))
 
 main()
